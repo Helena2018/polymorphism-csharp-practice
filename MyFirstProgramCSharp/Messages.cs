ac386a8334385f1683a80a117f292136f917dbb6
Helena2018 <helena2020liu@gmail.com>
2023-06-30 19:46:02 +1200

    first commit

---
+++ MyFirstProgramCSharp/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MyFirstProgramCSharp
+{
+	public class Messages
+	{
+		public static void Hello()
+		{
+			Console.WriteLine("Hello!");
+		}
+		public static void Waiting()
+		{
+			Console.WriteLine("Waiting...");
+		}
+		public static void Bye()
+		{
+			Console.WriteLine("Bye");
+		}
+	}
+}
+
---
+++ MyFirstProgramCSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MyFirstProgramCSharp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bycicle bycicle = new Bycicle();
+        Boat boat = new Boat();
+
+        Vehicle[] vehicles = {car, bycicle, boat};
+
+        foreach(Vehicle vehicle in vehicles)
+        {
+            vehicle.Go();
+        }
+       
+
+        Console.ReadKey(); 
+    }
+    
+}
+class Vehicle
+{
+    public virtual void Go()
+    {
+
+    }
+    
+}
+class Car : Vehicle
+{
+    public override void Go()
+    {
+        Console.WriteLine("The car is going!");
+    }
+}
+class Bycicle : Vehicle
+{
+    public override void Go()
+    {
+        Console.WriteLine("The bycicle is going!");
+    }
+}
+class Boat : Vehicle
+{
+    public override void Go()
+    {
+        Console.WriteLine("The boat is going!");
+    }
+}
